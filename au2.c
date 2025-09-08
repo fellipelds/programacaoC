53b063096c358e8b00c54de96c8f4a87451c1a5b
Fellipe Lima <fellipelima199@gmail.com>
2025-09-08 13:38:13 -0400

    Aula

---
+++ au2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 18;
+    int quantidade = 2;
+    float altura = 2,5;
+    char letra = 's';
+    char nome[7] = "Fellipe";
+
+    printf("A idade do %s é: %d./n", nome, idade);
+    
+}
